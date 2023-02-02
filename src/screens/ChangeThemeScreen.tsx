4be89a15e1c9d3471e14b2c95de3b7f909f1eac2
Diego Urrego <diegourrg@gmail.com>
2023-02-02 17:41:26 -0500

    Se crea el contexto de la pp para manejar el tema. Refactoricé una parte de la aplicación por el uso del contexto.

--- src/screens/ChangeThemeScreen.tsx
+++ src/screens/ChangeThemeScreen.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import {HeaderTitle} from '../components';
 import {styles} from '../theme/appTheme';
+import {ThemeContext} from '../context/themeContext/ThemeContext';
 
 export const ChangeThemeScreen = () => {
+  const {setDarkTheme} = useContext(ThemeContext);
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Change Theme" />
-      <TouchableOpacity style={screenStyles.button} activeOpacity={0.8}>
+      <TouchableOpacity
+        style={screenStyles.button}
+        activeOpacity={0.8}
+        onPress={setDarkTheme}>
         <Text style={screenStyles.buttonText}>Light / Dark</Text>
       </TouchableOpacity>
     </View>
