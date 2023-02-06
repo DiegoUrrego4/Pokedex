ea8a6b627a8c178bb5164c609078d781f11fa7af
Diego Urrego <diegourrg@gmail.com>
2023-02-06 15:12:58 -0500

    Se usa el método loadPokemons para producir un infinite scroll y se crea el <FlatList /> para mostrar la fotografía de los pokemons.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,57 @@
 import React from 'react';
-import {Image, Text} from 'react-native';
+import {
+  FlatList,
+  Image,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
 import {styles} from '../theme/appTheme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {usePokemonPaginated} from '../hooks';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
-  const {simplePokemonList} = usePokemonPaginated();
+  const {simplePokemonList, loadPokemons} = usePokemonPaginated();
   return (
     <>
       <Image
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-      <Text style={{...styles.globalMargin, ...styles.title, top: top + 20}}>
-        Pokedex
-      </Text>
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => (
+          <Image source={{uri: item.picture}} style={homeStyles.pokemonImage} />
+        )}
+        // ListHeaderComponent={
+        //   <Text
+        //     style={{...styles.globalMargin, ...styles.title, top: top + 20}}>
+        //     Pokedex
+        //   </Text>
+        // }
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={
+          <ActivityIndicator
+            style={homeStyles.activityIndicator}
+            size={20}
+            color="grey"
+          />
+        }
+      />
     </>
   );
 };
+
+const homeStyles = StyleSheet.create({
+  pokemonImage: {
+    width: 100,
+    height: 100,
+  },
+  activityIndicator: {
+    height: 100,
+  },
+});
