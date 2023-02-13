55b6a95cb15d2f632d3e27001d988798f31ef7a0
Diego Urrego <diegourrg@gmail.com>
2023-02-13 15:11:24 -0500

    Se crea customHook usePokemonSearch y se implementa en la vista <SearchScren />.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useAnimation';
 export * from './usePokemon';
 export * from './usePokemonPaginated';
+export * from './usePokemonSearch';
--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,29 @@
-import {StyleSheet, View, Platform} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Platform,
+  ActivityIndicator,
+  Text,
+  FlatList,
+} from 'react-native';
 import React from 'react';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {SearchInput} from '../components';
+import {PokemonCard, SearchInput} from '../components';
+import {usePokemonSearch} from '../hooks';
+import {styles as globalStyles} from '../theme/appTheme';
 
 export const SearchScreen = () => {
   const {top} = useSafeAreaInsets();
+  const {isFetching, simplePokemonList} = usePokemonSearch();
+
+  if (isFetching) {
+    return (
+      <View style={{...styles.activityContainer}}>
+        <ActivityIndicator size={50} color="grey" />
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
 
   return (
     <View
@@ -13,13 +32,39 @@ export const SearchScreen = () => {
         marginTop: Platform.OS === 'ios' ? top : top + 10,
       }}>
       <SearchInput />
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        // Header
+        ListHeaderComponent={
+          <Text
+            style={{
+              ...globalStyles.title,
+              ...globalStyles.globalMargin,
+              ...styles.titleList,
+            }}>
+            Pokedex
+          </Text>
+        }
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  activityContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     marginHorizontal: 20,
+  },
+  titleList: {
+    paddingBottom: 10,
   },
 });
